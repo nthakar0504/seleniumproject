f2c56fc85c8f495106389c456a6d06022943e4d9
PC <PC@DESKTOP-NV6AV6F>
2023-10-16 20:51:04 +0530

    changes made in basePage.java

--- src/main/java/Pages/BasePage.java
+++ src/main/java/Pages/BasePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {   // Open close principle Open for extention, close for modification
-							
+							// new changes
 	WebDriver driver;
 	
 	public BasePage(WebDriver driver) {
